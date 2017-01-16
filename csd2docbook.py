9d204f09d881052ab0c786acb0607be738180d5d
Nathan Whetsell <nathan.whetsell@gmail.com>
2017-01-16 13:10:18 -0500

    Remove unused imports

--- csd2docbook.py
+++ csd2docbook.py
@@ -17,7 +17,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-import getopt, glob, os, sys
+import glob
 from pygments import highlight
 from pygments.formatter import Formatter
 from pygments.lexers import CsoundDocumentLexer, CsoundOrchestraLexer
